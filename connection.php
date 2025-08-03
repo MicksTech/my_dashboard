e8fd046e350cc3c6ece2223c58e3674fb38a0e94
Micks-Tech <mickstech060924@gmail.com>
2025-08-03 12:00:59 +0800

    connect-commit

--- connection.php
+++ connection.php
@@ -1,3 +1,11 @@
 <?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'db_report';
 
+$conn = new mysqli ($host, $user, $password, $dbname);
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
 ?>
