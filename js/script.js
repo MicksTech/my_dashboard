efd6299e384ec428453d6c63fe8fd67368f560c8
Micks-Tech <mickstech060924@gmail.com>
2025-08-03 12:31:49 +0800

    frontcommit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-document.getElementById("toggleBtn").addEventListener("click", function () {
-  document.getElementById("navlist").classList.toggle("active");
-  document.getElementById("header").classList.toggle("active");
+const toggleBtn = document.getElementById('toggleBtn');
+const closeBtn = document.getElementById('closeBtn');
+const header = document.getElementById('header');
+const navlist = document.getElementById('navlist');
+
+toggleBtn.addEventListener('click', () => {
+    header.classList.toggle('active');
+    navlist.classList.toggle('active');
 });
 
-document.querySelectorAll("#navlist a").forEach((item) => {
-  item.addEventListener("click", function () {
-    document.getElementById("navlist").classList.remove("active");
-    document.getElementById("header").classList.remove("active");
-  });
+closeBtn.addEventListener('click', () => {
+    header.classList.remove('active');
+    navlist.classList.remove('active');
 });
+
+document.querySelectorAll('#navlist a').forEach((item) => {
+    item.addEventListener('click', () => {
+        header.classList.remove('active');
+        navlist.classList.remove('active');
+    })
+})
